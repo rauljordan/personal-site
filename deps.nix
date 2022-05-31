6e8fc2ae3d3dc58140ecfa2f14d255727e4bb7d8
Raul Jordan <raul@prysmaticlabs.com>
2022-05-30 22:37:23 -0500

    fix up

--- deps.nix
+++ deps.nix
@@ -1,12 +1,32 @@
 # file generated from go.mod using vgo2nix (https://github.com/nix-community/vgo2nix)
 [
   {
-    goPackagePath = "github.com/gomarkdown/markdown";
+    goPackagePath = "github.com/alecthomas/chroma";
     fetch = {
       type = "git";
-      url = "https://github.com/gomarkdown/markdown";
-      rev = "2372b9aa33e5";
-      sha256 = "0kc61nc2rs2gsp18fhy98y91mrs9n3ridfjvf3v2ffnhx9dx5a2w";
+      url = "https://github.com/alecthomas/chroma";
+      rev = "v0.10.0";
+      sha256 = "0hjzb61m5lzx95xss82wil9s8f9hbw1zb3jj73ljfwkq5lqk76zq";
+      moduleDir = "";
+    };
+  }
+  {
+    goPackagePath = "github.com/davecgh/go-spew";
+    fetch = {
+      type = "git";
+      url = "https://github.com/davecgh/go-spew";
+      rev = "v1.1.1";
+      sha256 = "0hka6hmyvp701adzag2g26cxdj47g21x6jz4sc6jjz1mn59d474y";
+      moduleDir = "";
+    };
+  }
+  {
+    goPackagePath = "github.com/dlclark/regexp2";
+    fetch = {
+      type = "git";
+      url = "https://github.com/dlclark/regexp2";
+      rev = "v1.4.0";
+      sha256 = "1irfv89b7lfkn7k3zgx610ssil6k61qs1wjj31kvqpxb3pdx4kry";
       moduleDir = "";
     };
   }
@@ -51,12 +71,72 @@
     };
   }
   {
+    goPackagePath = "github.com/pmezard/go-difflib";
+    fetch = {
+      type = "git";
+      url = "https://github.com/pmezard/go-difflib";
+      rev = "v1.0.0";
+      sha256 = "0c1cn55m4rypmscgf0rrb88pn58j3ysvc2d0432dp3c6fqg6cnzw";
+      moduleDir = "";
+    };
+  }
+  {
+    goPackagePath = "github.com/stretchr/objx";
+    fetch = {
+      type = "git";
+      url = "https://github.com/stretchr/objx";
+      rev = "v0.1.0";
+      sha256 = "19ynspzjdynbi85xw06mh8ad5j0qa1vryvxjgvbnyrr8rbm4vd8w";
+      moduleDir = "";
+    };
+  }
+  {
+    goPackagePath = "github.com/stretchr/testify";
+    fetch = {
+      type = "git";
+      url = "https://github.com/stretchr/testify";
+      rev = "v1.7.0";
+      sha256 = "0ixgjsvafr3513pz3r6pmgk074s2dxkll0dadvl25gkf30rkmh10";
+      moduleDir = "";
+    };
+  }
+  {
+    goPackagePath = "github.com/tj/assert";
+    fetch = {
+      type = "git";
+      url = "https://github.com/tj/assert";
+      rev = "ee03d75cd160";
+      sha256 = "17wajnzzrm4xhndyqycw7rvsxr8cgrwdfyb7iachfvxkdh34na2v";
+      moduleDir = "";
+    };
+  }
+  {
+    goPackagePath = "github.com/tj/go-naturaldate";
+    fetch = {
+      type = "git";
+      url = "https://github.com/tj/go-naturaldate";
+      rev = "v1.3.0";
+      sha256 = "12d7nf1jd7nk9r8ifn1hr21a7m4yb1garmiw2grrsi5zsqsh2jb1";
+      moduleDir = "";
+    };
+  }
+  {
     goPackagePath = "github.com/yuin/goldmark";
     fetch = {
       type = "git";
       url = "https://github.com/yuin/goldmark";
       rev = "v1.4.12";
       sha256 = "169cqjv1gn42qhk1kjyz4ihjhhgd5h12nj482j7v6m3vr071a7w7";
+      moduleDir = "";
+    };
+  }
+  {
+    goPackagePath = "github.com/yuin/goldmark-highlighting";
+    fetch = {
+      type = "git";
+      url = "https://github.com/yuin/goldmark-highlighting";
+      rev = "594be1970594";
+      sha256 = "0vy75xz2lmsmi694ikd94k9sgpwpnp0n82lvil80jn1854vxva6n";
       moduleDir = "";
     };
   }
@@ -87,6 +167,16 @@
       url = "https://gopkg.in/yaml.v2";
       rev = "v2.4.0";
       sha256 = "1pbmrpj7gcws34g8vwna4i2nhm9p6235piww36436xhyaa10cldr";
+      moduleDir = "";
+    };
+  }
+  {
+    goPackagePath = "gopkg.in/yaml.v3";
+    fetch = {
+      type = "git";
+      url = "https://gopkg.in/yaml.v3";
+      rev = "9f266ea9e77c";
+      sha256 = "1bbai3lzb50m0x2vwsdbagrbhvfylj9k1m32hgbqwldqx4p9ay35";
       moduleDir = "";
     };
   }
